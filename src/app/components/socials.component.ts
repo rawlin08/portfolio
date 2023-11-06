88daab44211b586f953ebc12903b8a339be7605b
Alec Rawlins <alec12999@yahoo.com>
2023-11-06 13:10:34 -0500

    open new tab when clicking on socials

--- src/app/components/socials.component.ts
+++ src/app/components/socials.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   template: `
   <h1>rawlin08</h1>
   <div class="socials">
-    <a href="https://github.com/rawlin08"><svg class="icon"><use href="#githubIcon"></use></svg></a>
-    <a href="https://www.linkedin.com/in/alec-rawlins-556874170/"><svg class="icon"><use href="#linkedinIcon"></use></svg></a>
+    <a href="https://github.com/rawlin08" target="_blank"><svg class="icon"><use href="#githubIcon"></use></svg></a>
+    <a href="https://www.linkedin.com/in/alec-rawlins-556874170/" target="_blank"><svg class="icon"><use href="#linkedinIcon"></use></svg></a>
   </div>
   `,
   styles: [`
